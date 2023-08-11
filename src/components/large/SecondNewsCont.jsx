c178a24e73271526f1622aaf57b242100f302761
marcoglp98 <marcoglp98@gmail.com>
2023-08-11 12:35:47 +0200

    feat: add SecondNewsCont component

--- src/components/large/SecondNewsCont.jsx
+++ src/components/large/SecondNewsCont.jsx
@@ -41,6 +41,39 @@ const SecondNewsCont = () => {
       <div className=" bg-red-500 max-h-[40vh] col-span-1">
         <BlogBanner></BlogBanner>
       </div>
+      <div className=" bg-red-500 max-h-[80vh] col-span-6">
+        <NewsCard sizes="h-[80vh] w-[100vw]" image={roccella}></NewsCard>
+      </div>
+      <div className=" bg-red-500 max-h-[60vh] col-span-1 ">
+        <NewsCard sizes="h-[60vh] w-[100vw]" image={roccella}></NewsCard>
+      </div>
+      <div className=" bg-red-500 max-h-[60vh] col-span-1 ">
+        <NewsCard sizes="h-[60vh] w-[100vw]" image={roccella}></NewsCard>
+      </div>
+      <div className=" bg-red-500 max-h-[60vh] col-span-2 ">
+        <NewsCard sizes="h-[60vh] w-[100vw]" image={roccella}></NewsCard>
+      </div>
+      <div className=" bg-red-500 max-h-[60vh] w-[vw] col-span-2 ">
+        <NewsCard sizes="h-[60vh] w-[100vw]" image={roccella}></NewsCard>
+      </div>
+      <div className=" bg-red-500 max-h-[0vh] w-[vw] col-start-4 col-span-3 ">
+        <NewsCard sizes="h-[102vh] w-[100vw]" image={roccella}></NewsCard>
+      </div>
+      <div className=" bg-red-500 max-h-[25vh] col-start-1 col-end-4 ">
+        <NewsCard sizes="h-[25vh] w-[100vw]" image={roccella}></NewsCard>
+      </div>
+     
+      <div className=" bg-red-500 max-h-[25vh] w-[vw] col-start-1 col-end-4">
+        <NewsCard sizes="h-[25vh] w-[100vw]" image={roccella}></NewsCard>
+      </div>
+      <div className=" bg-red-500 max-h-[25vh] w-[vw] col-start-1 col-end-4 ">
+        <NewsCard sizes="h-[25vh] w-[100vw]" image={roccella}></NewsCard>
+      </div>
+      
+      <div className=" bg-red-500 max-h-[25vh] w-[vw] col-start-1 col-end-4 ">
+        <NewsCard sizes="h-[25vh] w-[100vw]" image={roccella}></NewsCard>
+      </div>
+     
     </div>
   );
 };
